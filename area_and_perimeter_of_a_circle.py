42c73efe69a44cac014dcf23cf0e7f56ee7b8e94
Ubuntu <ubuntu@ip-172-31-76-11.ec2.internal>
2020-11-26 17:49:45 +0000

    This program calculates area and perimeter of a circle

--- area_and_perimeter_of_a_circle.py
+++ area_and_perimeter_of_a_circle.py
@@ -4,14 +4,16 @@
 # Created on NOvember 2020
 # This program is meant to calculate area and perimeter of a circel
 # with a radius of 15mm
+import math
 
 
 def main():
-    '#this function calculates area and perimeter of a circle'
-    import math
+    # this function calculates area and perimeter of a circle
+
     rad = 15
-    print("Area is {}cm^2".format(math.pi*rad**2))
-    print("Perimeter is {}cm^2".format(math.pi*2*rad))
+    print("If a circle has a radius of 15mm")
+    print("The area is {}mm2".format(math.pi*rad**2))
+    print("And the perimeter is {}mm".format(math.pi*2*rad))
 
 
 if __name__ == "__main__":
